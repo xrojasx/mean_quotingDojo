18c6d26f55ddf6fbb8c30daf131d6e7c7c92a39b
Ricardo Rojas <xrojas@att.net>
2017-12-12 16:21:24 -0800

    fully modulariazed

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var express = require('express');
 var app = express();
 // Require body-parser (to receive post data from clients)
 var bodyParser = require('body-parser');
-// Integrate body-parser with our App
-var mongoose = require('mongoose');
 
 var path = require("path");
 
@@ -16,18 +14,13 @@ app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up database connection, Schema, model
-mongoose.connect('mongodb://localhost/quotes_data');
-
-var quoteSchema = new mongoose.Schema({
-name: String,
-quote: String
-});
-const Quote = mongoose.model('quotes', quoteSchema);
 
 // Point server to views
 app.set('views', path.join(__dirname, 'views'));
 // We're using ejs as our view engine
 app.set('view engine', 'ejs');
+
+require('./server/config/mongoose.js');
 
 var routes_setter = require('./server/config/routes.js');
 routes_setter(app);
